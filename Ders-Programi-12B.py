701fa63109ba04f01b18d82edbe9a6c11ddd6f16
Yiğit Çıtak <140903140+Yigit-2023@users.noreply.github.com>
2023-09-18 19:03:03 +0300

    Update Ders-Programi-12B.py
    1.1

--- Ders-Programi-12B.py
+++ Ders-Programi-12B.py
@@ -1,6 +1,6 @@
 #Yapımcı:
 #Yiğit Çıtak
-Version = "1.0"
+Version = "1.1"
 ders_list = """
 Ders/gün	 1	  2	  3	  4	  5	  6	  7	  8
 ------------------------------------------------------------------------------------------
@@ -17,12 +17,66 @@ Cuma	 |      SMAN4	SMAN4	STMAT1	STMAT1	SÇTDT	SÇTDT	SFIKO	SFIKO
 											|17.09.2023|	
 																	
 """
-#Kopyala Yapıştır:	1- 2- 3- 4- 5- 6- 7- 8-
-pazartesi = """\n1-M.Arapça 2-M.Arapça 3-Dini Müsiki 4-Adabı Muaşeret 5-Kalem 6-Kalem 7-Türk Dili edebiyat 8-Seçmeli Türk Dili Edebiyat"""
-sali = """\n1-Seçmeli Coğrafya 2-Seçmeli Coğrafya 3-Kuran 4-Kuran 5-Arapça 6-Beden 7-Türk Dili edebiyat 8-Türk Dili edebiyat"""
-carsamba = """\n1-Seçmeli Edebiyat 2-Seçmeli Edebiyat 3-İnkılap 4-İnkılap 5-İngilizce 6-İngilizce 7-Türk Dili edebiyat 8-Türk Dili edebiyat"""
-persembe = """\n1-Seçmeli Coğrafya 2-Seçmeli Coğrafya 3-İslam kültür ve medeniyeti 4-İslam kültür ve medeniyeti \n5-Dinler Tarihi	6-Dinler Tarihi	7-Kuran	8-Seçmeli Türk Dili Edebiyat"""
-cuma = """\n1-Seçmeli Mantık 2-Seçmeli Mantık 3-S.Temel Matematik 4-S.Temel Matematik 5-Seçmeli Türk Dili Edebiyat 6-Seçmeli Türk Dili Edebiyat 7-Seçmeli Fıkıh 8-Seçmeli Fıkıh"""
+adap = "Adabı Muaşeret "
+beden1 = "Beden "
+dm3 = "Dini Müsiki "
+ikmt2 = "İslam Kültür ve medeniyeti "
+ing4 = "İngilizce "
+ink3 = "T.C inkılap "
+kdt = "Dinler Tarihi "
+klm3 = "Kalem "
+kur = "Kuran "
+m_arp = "arapça "
+s_ted = "Seçmeli Türk ve dünya tarihi "
+s_cog = "Seçmeli coğrafya "
+sctdt = "Çağdaş türk ve dünya tarihi "
+sfiko = "Seçmeli fıkıh "
+man = "Mantık "
+mat = "Matematik "
+tde32 = "Türk Dili ve edebiyatı "
+
+#Ders saatleri
+ders1 = "08:40 - 09:20"
+ders2 = "09:40 - 10:20"
+ders3 = "10:30 - 11:10"
+ders4 = "11:20 - 12:00"
+ders5 = "12:10 - 12:50"
+ders6 = "13:40 - 14:20"
+ders7 = "14:30 - 15:10"
+ders8 = "15:20 - 16:00"
+
+#cuma günü
+
+c_ders1 = "08:40 - 09:20"
+c_ders2 = "09:30 - 10:10"
+c_ders3 = "10:20 - 11:00"
+c_ders4 = "11:10 - 11:50"
+c_ders5 = "12:00 - 12:40"
+c_ders6 = "13:50 - 14:30"
+c_ders7 = "14:40 - 15:20"
+c_ders8 = "15:30 - 16:10"
+
+
+
+
+at = "\n"
+ay = "\t"
+
+pazartesi = at+m_arp+at+m_arp+at+dm3+at+dm3+at+adap+at+klm3+at+klm3+at+tde32+at+sctdt
+sali = at+s_cog+at+s_cog+at+kur+at+kur+at+m_arp+at+beden1+at+tde32+at+tde32
+carsamba = at+s_ted+at+s_ted+at+ink3+at+ink3+at+ing4+at+ing4+at+tde32+at+tde32
+persembe = at+s_cog+at+s_cog+at+ikmt2+at+ikmt2+at+kdt+at+kdt+at+kur+at+sctdt
+cuma =at+man+at+man+at+mat+at+mat+at+sctdt+at+sctdt+at+sfiko+at+sfiko
+
+#Günler
+
+
+
+
+
+
+
+
 
 print("12-B Sınıfı Ders Programı\nSürüm:\t",Version,"\n")
 
@@ -34,23 +88,45 @@ while dongu_of_or_on:
 
 	ilk_girdi = input("\nHangi Günü incelemek istiyorsun?\n\n")
 
-	if ilk_girdi == "pazartesi" or ilk_girdi == "PAZARTESİ":
+	if ilk_girdi == "pazartesi" or ilk_girdi == "PAZARTESİ" or ilk_girdi == "Pazartesi":
 		print(pazartesi)
 		
 
-	elif ilk_girdi == "salı" or ilk_girdi == "SALI":
+	elif ilk_girdi == "salı" or ilk_girdi == "SALI" or ilk_girdi == "Salı":
 		print(sali)
 		
 
-	elif ilk_girdi == "çarşamba" or ilk_girdi == "ÇARŞAMBA":
+	elif ilk_girdi == "çarşamba" or ilk_girdi == "ÇARŞAMBA" or ilk_girdi == "Çarşamba":
 		print(carsamba)
 		
 
-	elif ilk_girdi == "perşembe" or ilk_girdi == "PERŞEMBE":
+	elif ilk_girdi == "perşembe" or ilk_girdi == "PERŞEMBE" or ilk_girdi == "Perşembe":
 		print(persembe)
 		
-	elif ilk_girdi == "cuma" or ilk_girdi == "CUMA":
+	elif ilk_girdi == "cuma" or ilk_girdi == "CUMA" or ilk_girdi == "Cuma":
 		print(cuma)
+
+
+		#Resetle
+	elif ilk_girdi == "sil" or ilk_girdi == "SİL" or ilk_girdi == "Sil":
+		print(at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at)
+		print(at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at+at)
+		print("12-B Sınıfı Ders Programı\nSürüm:\t",Version,"\n")
+		print(ders_list)
+
+
+
+
+	elif ilk_girdi == "Saat" or ilk_girdi == "SAAT" or ilk_girdi == "saat":
+		print("Ders saatleri:")
+		print("1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8")
+		print(ders1+ay+ders2+ay+ders3+ay+ders4+ay+ders5+ay+ders6+ay+ders7+ay+ders8)
+		print(at)
+		print("Cuma Günü:")
+		print("1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8")
+		print(c_ders1+ay+c_ders2+ay+c_ders3+ay+c_ders4+ay+c_ders5+ay+c_ders6+ay+c_ders7+ay+c_ders8)
+		print(at)
+
 
 	elif ilk_girdi == "Yapımcı":
 		print("\nYiğit Çıtak\n")
